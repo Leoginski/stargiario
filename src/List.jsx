0e36b8746d3f2f7c511e97da6408c6a628a38e43
Leoginki <leonardo.smoginski@gmail.com>
2017-11-18 00:25:25 -0200

    Filtros de busca.

--- src/List.jsx
+++ src/List.jsx
@@ -5,7 +5,8 @@ class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ''
+            search: '',
+            atribute: 'Nome'
         }
     }
 
@@ -13,16 +14,37 @@ class List extends Component {
         this.setState({search: event.target.value.substr(0, 20)});
     }
 
+    updateAtribute(event) {
+        this.setState({atribute: event.target.value});
+    }
+
     render() {
         let filteredPeople = this.props.people.filter(
             (people) => {
-                return people.name.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1;
+                if (this.state.atribute === 'Nome') {
+                    return people.name.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1;
+                }
+                if (this.state.atribute === 'Olhos') {
+                    return people.eye_color.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1;
+                }
+                if (this.state.atribute === 'Cabelos') {
+                    return people.hair_color.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1;
+                }
+                if (this.state.atribute === 'Sexo') {
+                    return people.gender.toLocaleLowerCase().indexOf(this.state.search.toLocaleLowerCase()) !== -1;
+                }
             }
         );
         return (
             <div className={'container'}>
                 <div className={'container bg-info rounded text-bold text-white'} style={{width: 250, height: 100}}>
                     <h5>Buscar</h5>
+                    <select value={this.state.atribute} onChange={this.updateAtribute.bind(this)}>
+                        <option>Nome</option>
+                        <option>Olhos</option>
+                        <option>Cabelos</option>
+                        <option>Sexo</option>
+                    </select>
                     <input value={this.state.search} onChange={this.updateSearch.bind(this)}/>
                 </div>
                 {
